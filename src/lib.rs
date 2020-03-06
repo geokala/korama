65b81ff7b60765f9b6f05e2076d34e8e7446b129
Madda <nobody@nowhere.local>
2020-03-06 22:01:17 +0000

    Move track to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+pub mod track;
+
 use id3::Tag;
-use std::cmp::Ordering;
 use std::ffi::OsStr;
 use std::fs::{File, read_to_string};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+
+pub use crate::track::Track;
 
 // Finding out what is valid in id3v2 tags turned out to be aggravating.
 // It might be possible for these to be used, but they will not display well
@@ -252,67 +255,5 @@ impl MusicLibrary {
             Ok(_) => {},
             Err(err) => panic!("Could not complete write to {}: {:#?}", library_path.display(), err),
         };
-    }
-}
-
-#[derive(Clone)]
-pub struct Track {
-    pub track_name: String,
-    pub artist: String,
-    pub album: String,
-    pub track_number: String,  // Yes, a string, because of hidden tracks on some albums
-    pub path: String,
-}
-
-impl Track {
-    fn order_by_track(&self, other: &Self) -> Ordering {
-        if self.track_name > other.track_name {
-            Ordering::Greater
-        } else if self.track_name < other.track_name {
-            Ordering::Less
-        } else {
-            // Artist name breaks ties
-            if self.artist > other.artist {
-                Ordering::Greater
-            } else {
-                Ordering::Less
-            }
-        }
-    }
-
-    fn order_by_artist_and_album(&self, other: &Self) -> Ordering {
-        if self.artist > other.artist {
-            Ordering::Greater
-        } else if self.artist < other.artist {
-            Ordering::Less
-        } else {
-            // Same artist
-            if self.album > other.album {
-                Ordering::Greater
-            } else if self.album < other.album {
-                Ordering::Less
-            } else {
-                // Same album
-                if self.track_number > other.track_number {
-                    Ordering::Greater
-                } else if self.track_number < other.track_number {
-                    Ordering::Less
-                } else {
-                    // Somebody forgot to put track numbers
-                    // We'll break ties on track name here
-                    if self.track_name > other.track_name {
-                        Ordering::Greater
-                    } else {
-                        Ordering::Less
-                    }
-                }
-            }
-        }
-    }
-}
-
-impl PartialEq for Track {
-    fn eq(&self, other: &Self) -> bool {
-        self.track_name == other.track_name && self.artist == other.artist && self.album == other.album && self.track_number == other.track_number && self.path == other.path
     }
 }
---
+++ src/track.rs
@@ -0,0 +1,63 @@
+use std::cmp::Ordering;
+
+#[derive(Clone)]
+pub struct Track {
+    pub track_name: String,
+    pub artist: String,
+    pub album: String,
+    pub track_number: String,  // Yes, a string, because of hidden tracks on some albums
+    pub path: String,
+}
+
+impl Track {
+    pub fn order_by_track(&self, other: &Self) -> Ordering {
+        if self.track_name > other.track_name {
+            Ordering::Greater
+        } else if self.track_name < other.track_name {
+            Ordering::Less
+        } else {
+            // Artist name breaks ties
+            if self.artist > other.artist {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        }
+    }
+
+    pub fn order_by_artist_and_album(&self, other: &Self) -> Ordering {
+        if self.artist > other.artist {
+            Ordering::Greater
+        } else if self.artist < other.artist {
+            Ordering::Less
+        } else {
+            // Same artist
+            if self.album > other.album {
+                Ordering::Greater
+            } else if self.album < other.album {
+                Ordering::Less
+            } else {
+                // Same album
+                if self.track_number > other.track_number {
+                    Ordering::Greater
+                } else if self.track_number < other.track_number {
+                    Ordering::Less
+                } else {
+                    // Somebody forgot to put track numbers
+                    // We'll break ties on track name here
+                    if self.track_name > other.track_name {
+                        Ordering::Greater
+                    } else {
+                        Ordering::Less
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl PartialEq for Track {
+    fn eq(&self, other: &Self) -> bool {
+        self.track_name == other.track_name && self.artist == other.artist && self.album == other.album && self.track_number == other.track_number && self.path == other.path
+    }
+}
