a23c53abc2c9e669219e925fa22bba375454ec43
Madda <nobody@nowhere.local>
2020-03-07 17:36:41 +0000

    Implement playlist track add+list

--- src/playlist.rs
+++ src/playlist.rs
@@ -1,15 +1,27 @@
+use crate::track::Track;
+
 pub struct Playlist {
     name: String,
+    tracks: Vec<Track>,
 }
 
 impl Playlist {
     pub fn new(name: String) -> Playlist {
         Playlist{
             name,
+            tracks: Vec::new(),
         }
     }
 
     pub fn get_name(&self) -> &str {
         &self.name
+    }
+
+    pub fn get_tracks(self) -> Vec<Track> {
+        self.tracks
+    }
+
+    pub fn add_track(&mut self, track: Track) {
+        &self.tracks.push(track);
     }
 }
