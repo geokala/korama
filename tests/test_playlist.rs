62e0302d23cdab009a50b63db206f9b3c93113a4
Madda <nobody@nowhere.local>
2020-03-07 17:17:48 +0000

    Add test for adding and retrieving playlist tracks

--- tests/test_playlist.rs
+++ tests/test_playlist.rs
@@ -8,3 +8,30 @@ fn create_playlist() {
 
     assert!(playlist.get_name() == playlist_name);
 }
+
+#[test]
+fn add_tracks() {
+    let mut playlist = korama::Playlist::new(String::from("Test playlist"));
+
+    let example_track_1 = korama::Track {
+        track_name: String::from("Example track"),
+        artist: String::from("Example artist"),
+        album: String::from("Example album"),
+        track_number: String::from(""),
+        path: String::from("/some/path"),
+    };
+    let example_track_2 = korama::Track {
+        track_name: String::from("Extrample track"),
+        artist: String::from("Extrample artist"),
+        album: String::from("Extrample album"),
+        track_number: String::from("4"),
+        path: String::from("/some/other/path"),
+    };
+
+    playlist.add_track(example_track_1.clone());
+    playlist.add_track(example_track_2.clone());
+
+    let expected = vec!(example_track_1, example_track_2);
+
+    assert!(playlist.get_tracks() == expected);
+}
