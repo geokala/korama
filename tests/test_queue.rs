fb872b061ccec1ce7c3a07b731943c530a7ab4d3
Madda <nobody@nowhere.local>
2020-04-08 17:25:44 +0100

    Add initial queue test

---
+++ tests/test_queue.rs
@@ -0,0 +1,13 @@
+use korama;
+
+
+#[test]
+fn set_queue_playlist() {
+    let playlist = korama::Playlist::new(String::from("Test playlist for queue"));
+
+    let mut queue = korama::Queue::new();
+
+    queue.use_playlist(&playlist);
+
+    assert_eq!(queue.get_playlist().get_name(), String::from("Test playlist for queue"));
+}
