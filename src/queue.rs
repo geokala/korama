04c8ec1527de82606c74e1ef3369ed7fdc31bdc1
Madda <nobody@nowhere.local>
2020-05-03 10:58:41 +0100

    This is not how to thread... keep trying

--- src/queue.rs
+++ src/queue.rs
@@ -3,6 +3,7 @@ use crate::track::Track;
 use std::fs::File;
 use std::io::BufReader;
 use rodio::Sink;
+use std::thread;
 
 use crate::shared::Saveable;
 
@@ -11,6 +12,7 @@ pub struct Queue {
     playlist: Option<Playlist>,
     history: Vec<Track>,
     sink: rodio::Sink,
+    is_playing: bool,
 }
 
 impl Queue {
@@ -20,6 +22,7 @@ impl Queue {
             playlist: None,
             history: Vec::new(),
             sink: rodio::Sink::new(&device),
+            is_playing: false,
         }
     }
 
@@ -31,17 +34,29 @@ impl Queue {
         self.playlist
     }
 
-    pub fn play(&mut self) {
-        for track in self.playlist.as_mut().unwrap() {
-            let file = File::open(&track.path).unwrap();
-            let source = match rodio::Decoder::new(BufReader::new(file)) {
-                Ok(src) => src,
-                // TODO: This should be logging, not panicking.
-                Err(err) => panic!("Could not play file: {}: {:#?}", &track.path, err),
+    fn play_handler(&mut self) {
+        while self.is_playing {
+            let next_track = self.playlist.as_mut().unwrap().next();
+            match next_track {
+                Some(track) => {
+                    self.history.push(track.clone());
+                    let file = File::open(&track.path).unwrap();
+                    let source = match rodio::Decoder::new(BufReader::new(file)) {
+                        Ok(src) => src,
+                        // TODO: This should be logging, not panicking.
+                        Err(err) => panic!("Could not play file: {}: {:#?}", &track.path, err),
+                    };
+                    self.sink.append(source);
+                    self.sink.play();
+                    self.sink.sleep_until_end();
+                },
+                None => self.is_playing = false,
             };
-            self.sink.append(source);
         };
-        self.sink.play()
+    }
+
+    pub fn play(&mut self) {
+        thread::spawn(self.play_handler());
     }
 
     pub fn get_history(&self) -> Vec<Track> {
